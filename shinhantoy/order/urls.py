f14e0113ff09ed510e868170d2e78b9b9508fb6d
datadbs <dj9034kr@gmail.com>
2023-01-26 16:49:17 +0900

    feat: add order search

--- shinhantoy/order/urls.py
+++ shinhantoy/order/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path("/<int:order_id>/comment", views.CommentListView.as_view()),
     path("/comment", views.CommentCreateView.as_view()),
     path("/<int:pk>", views.OrderDetailView.as_view()),
-    path("", views.OrderListView.as_view()),
+    path("/", views.OrderListView.as_view()),
 ]
